1591d51d38fa9c117c3b4384b077c1b2cdfd2b0b
JIMLIU <jim.t90615@gmail.com>
2024-02-17 09:48:36 +0800

    new branch test

--- main.c
+++ main.c
@@ -1,40 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*輸出 1 到 100 之間的奇數*/
-int add(int,int);
-int m(int,int);
-int waru(int,int);
-int n(int,int);
 int main()
 {
-	 int a,b;
-	a=5;
-	b=5;
-	printf("a+b=%d",add(a,b));
-	printf("a-b=%d",m(a,b));
-	printf("a*b=%d",n(a,b));
-	printf("a/b=%d",waru(a,b));
 	system("pause");
 	 return 0;
-}
-int add(int a,int b)
-{
-	return a+b;
-}
-int m(int a,int b)
-{
-
-return a-b;
-}
-int n(int a,int b)
-{
-        return a*b;
-}
-
-
-
- 
-int waru(int a,int b)
-{
-        return a/b;
 }
